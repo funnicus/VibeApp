6d021ac10376cbccb3609b6ce214fcf2588fe0f6
Juhana Kuparinen <jmkupa@utu.fi>
2020-11-08 00:48:38 +0200

    iconi ja title

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
     return (
       <header>
           <hgroup>
-                <h1>Community</h1>
+                <h1>Vibe app</h1>
                 <h2>Keeping you connected during the pandemic!</h2>
                 <p>Send some positive vibes to your fellow students!</p>
           </hgroup>
